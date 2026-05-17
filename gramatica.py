3c44b81de1c4f3c3b7078e2e3aa69b7680d3c7db
SantiReRo <santiago.reyes.rodriguez@correounivalle.edu.co>
2026-05-16 20:07:03 -0500

    Ordenar el codigo

--- gramatica.py
+++ gramatica.py
@@ -6,11 +6,7 @@ gramatica = {
         ["INTENCION", "REQUERIMIENTO", "USO_FINAL"],                   # busco un teléfono xiaomi para juegos pesados
         ["INTENCION", "REQUERIMIENTO"]                                 # quiero un teléfono gama media
     ],
-    
     "INTENCION": [["busco"], ["quiero"], ["necesito"]],
-    "ARTICULO": [["un"], ["una"], ["algo", "con"], ["algo"]],
-    "CANTIDAD": [["mucha"], ["bastante"], ["poca"], ["bastante", "poca"], ["poco"]],
-    
     "REQUERIMIENTO": [
         ["ARTICULO", "COMPONENTE", "MARCA", "GAMA"], # un teléfono samsung gama alta
         ["ARTICULO", "COMPONENTE", "MARCA"],         # un teléfono xiaomi
@@ -20,19 +16,18 @@ gramatica = {
         ["CANTIDAD", "RAM"],                         # mucha memoria
         ["CANTIDAD", "ALMACENAMIENTO"],              # mucha memoria
     ],
-    
-    "COMPONENTE": [["teléfono"], ["celular"], ["móvil"], ["procesador"]],
-    "RAM": [["memoria"]],             
-    "ALMACENAMIENTO": [["memoria"]],  
-    
-    "MARCA": [["samsung"], ["xiaomi"], ["motorola"], ["apple"], ["poco"], ["realme"], ["infinix"], ["rogphone"], ["redmagic"]],
-    "GAMA": [["gama", "alta"], ["gama", "media"], ["gama", "baja"]],
-    
-    "ESPECIFICACION": [
-        ["mediatek"], ["snapdragon"], ["exynos"], ["tensor"], 
-        ["soporte", "magisk"], ["soporte", "root"]
-    ],
-    
+        "ARTICULO": [["un"], ["una"], ["algo", "con"], ["algo"]],
+        "CANTIDAD": [["mucha"], ["bastante"], ["poca"], ["bastante", "poca"], ["poco"]],
+        "COMPONENTE": [["teléfono"], ["celular"], ["móvil"], ["procesador"]],
+        "RAM": [["memoria"]],             
+        "ESPECIFICACION": [
+            ["mediatek"], ["snapdragon"], ["exynos"], ["tensor"], 
+            ["soporte", "magisk"], ["soporte", "root"]
+        ],
+        "MARCA": [["samsung"], ["xiaomi"], ["motorola"], ["apple"], ["poco"], ["realme"], ["infinix"], ["rogphone"], ["redmagic"]],
+        "GAMA": [["gama", "alta"], ["gama", "media"], ["gama", "baja"]],
+        "ALMACENAMIENTO": [["memoria"]],  
+    ############################################################################################
     "CARACTERISTICA": [
         ["que", "no", "se", "caliente"], 
         ["que", "sea", "rápido"],
@@ -40,11 +35,12 @@ gramatica = {
         ["que", "sea", "eficiente"],
         ["que", "sea", "veloz"],
     ],
-    
     "USO_FINAL": [
         ["que", "sea", "bueno", "para", "JUEGO"], 
         ["para", "JUEGO"],
         ["para", "jugar"],
     ],
-    "JUEGO": [["juegos", "pesados"], ["minecraft"], ["call", "of", "duty"], ["fortnite"], ["free", "fire"], ["mobile", "legends"], ["pubg", "mobile"]]
+        "JUEGO": [["juegos", "pesados"], ["minecraft"], ["call", "of", "duty"], ["fortnite"], ["free", "fire"], ["mobile", "legends"], ["pubg", "mobile"]],
+    
+
 }
